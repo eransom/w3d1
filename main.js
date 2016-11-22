0ef901223001c669933f62ded6e01c8920bcfa89
Eric Ransom <ericransom@Erics-MBP.cfl.rr.com>
2016-11-21 23:03:53 -0500

    add three files

---
+++ main.js
@@ -0,0 +1,27 @@
+
+$( ".dropDown" ).click(function() {
+  $( ".sub-menu" ).slideToggle( "slow", function() {
+  });
+});
+
+
+
+
+// $(document).ready(function() {
+//     $( '.dropDown' ).click(
+//         function(){
+//             $(this).children('.sub-menu').slideDown(200);
+//         },
+//         function(){
+//             $(this).children('.sub-menu').slideUp(200);
+//         }
+//     );
+// });
+
+
+// $(document).ready(function() {
+//     $( '.dropDown' ).click(
+//         function(){
+//             $(this).find('ul>li').slideToggle(200);
+//         });
+// });
